482fbfece7e996ae6b12600391a1831e6f34ccf9
Yoonjung Ahn <yoonjungaa@gmail.com>
2021-03-04 17:36:51 -0500

    Added realm check

--- plot_precipitation_climatology.py
+++ plot_precipitation_climatology.py
@@ -5,7 +5,8 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import numpy as np
 import cmocean
-
+import pdb
+import cmdline_provenance as cmdprov
 
 
 def convert_pr_units(darray):
@@ -20,73 +21,68 @@ def convert_pr_units(darray):
     darray.attrs['units'] = 'mm/day'
     
     return darray
-
-
-def create_plot(clim, model, season, gridlines=False):
-    """Plot the precipitation climatology.
-    
-    Args:
-      clim (xarray.DataArray): Precipitation climatology data
-      model (str): Name of the climate model
-      season (str): Season
-      
-    Kwargs:  
-      gridlines (bool): Select whether to plot gridlines    
-    
-    """
-        
-    fig = plt.figure(figsize=[12,5])
-    ax = fig.add_subplot(111, projection=ccrs.PlateCarree(central_longitude=180))
-    clim.sel(season=season).plot.contourf(ax=ax,
-                                          levels=np.arange(0, 13.5, 1.5),
-                                          extend='max',
-                                          transform=ccrs.PlateCarree(),
-                                          cbar_kwargs={'label': clim.units},
-                                          cmap=cmocean.cm.haline_r)
-    ax.coastlines()
-    if gridlines:
-        plt.gca().gridlines()
-    
-    title = f'{model} precipitation climatology ({season})'
-    plt.title(title)
-
 
 def main(inargs):
     """Run the program."""
 
     dset = xr.open_dataset(inargs.pr_file)
-    
+   
     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
     clim = convert_pr_units(clim)
 
-    create_plot(clim, dset.attrs['source_id'], inargs.season)
-    plt.savefig(inargs.output_file, dpi=200)
+    image_format = inargs.output_file.split('.')[-1]
+    assert image_format == 'png', 'Only valid output format is .png'
 
-
-if __name__ == '__main__':
-    description='Plot the precipitation climatology.'
-    parser = argparse.ArgumentParser(description=description)
+    new_log = cmdprov.new_log(infile_history={inargs.pr_file: dset.attrs['history']})
+    #pdb.set_trace()
+    plt.savefig(inargs.output_file, metadata={'History': new_log}, dpi=200)
+    new_log= new_log.replace('\n',' END ')
     
-    parser.add_argument("pr_file", type=str, help="Precipitation data file")
-    parser.add_argument("season", type=str, help="Season to plot")
-    parser.add_argument("output_file", type=str, help="Output file name")
+    if inargs.mask:
+        sftlf_file, realm = inargs.mask
+        clim = apply_mask(clim, sftlf_file, realm)
 
-    args = parser.parse_args()
+# def main(inargs):
+#     """Run the program."""
+
+#     dset = xr.open_dataset(inargs.pr_file)
     
-    main(args)
+#     clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
+#     clim = convert_pr_units(clim)
+    
+#     image_format = inargs.output_file.split('.')[-1]
+#     assert image_format == 'png', 'Only valid output format is .png'
 
-def convert_pr_units(darray):
-    """Convert kg m-2 s-1 to mm day-1.
-   
+#     new_log = cmdprov.new_log(infile_history={inargs.pr_file: dset.attrs['history']})
+#     pdb.set_trace()
+#     plt.savefig(inargs.output_file, metadata={'History': new_log}, dpi=200)
+#     new_log= new_log.replace('\n',' END ')
+    
+#     create_plot(clim, dset.attrs['source_id'], inargs.season)
+#     plt.savefig(inargs.output_file, dpi=200)
+
+
+
+def apply_mask(darray, sftlf_file, realm):
+    """Mask ocean or land using a sftlf (land surface fraction) file.
+    
     Args:
-      darray (xarray.DataArray): Precipitation data
+      darray (xarray.DataArray): Data to mask
+      sftlf_file (str): Land surface fraction file
+      realm (str): Realm to mask
     
     """
-    
-    darray.data = darray.data * 86400
-    darray.attrs['units'] = 'mm/day'
    
-    return darray
+    dset = xr.open_dataset(sftlf_file)
+    assert realm in ['land', 'ocean'], """Valid realms are 'land' or 'ocean'"""   
+
+    #pdb.set_trace()    
+    if realm == 'land':
+        masked_darray = darray.where(dset['sftlf'].data < 50)
+    else:
+        masked_darray = darray.where(dset['sftlf'].data > 50)   
+   
+    return masked_darray
 
 
 def create_plot(clim, model, season, gridlines=False, levels=None):
@@ -122,18 +118,6 @@ def create_plot(clim, model, season, gridlines=False, levels=None):
     plt.title(title)
 
 
-def main(inargs):
-    """Run the program."""
-
-    dset = xr.open_dataset(inargs.pr_file)
-    
-    clim = dset['pr'].groupby('time.season').mean('time', keep_attrs=True)
-    clim = convert_pr_units(clim)
-
-    create_plot(clim, dset.attrs['source_id'], inargs.season,
-                gridlines=inargs.gridlines, levels=inargs.cbar_levels)
-    plt.savefig(inargs.output_file, dpi=200)
-
 
 if __name__ == '__main__':
     description='Plot the precipitation climatology.'
@@ -142,7 +126,7 @@ if __name__ == '__main__':
     parser.add_argument("pr_file", type=str, help="Precipitation data file")
     parser.add_argument("season", type=str, help="Season to plot")
     parser.add_argument("output_file", type=str, help="Output file name")
-
+    parser.add_argument("--mask", type=str, nargs=2,metavar=('SFTLF_FILE', 'REALM'), default=None,help="""Provide sftlf file and realm to mask ('land' or 'ocean')""")
     parser.add_argument("--gridlines", action="store_true", default=False,
                         help="Include gridlines on the plot")
     parser.add_argument("--cbar_levels", type=float, nargs='*', default=None,
